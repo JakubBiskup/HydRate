584758fcbf11dd9de2937c90cba5bad478ee9bb2
Jakub Biskup <biskup.jakub@gmail.com>
2020-12-11 13:01:51 +0100

    Configure connection with mariaDB, create basic water model

---
+++ src/main/java/com/example/hydrate/model/Water.java
@@ -0,0 +1,70 @@
+package com.example.hydrate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Water {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private String company;
+    private String source;
+    @Column(length = 3000)
+    private String description;
+    private int minerals;  //        mg/l
+
+    public Water() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMinerals() {
+        return minerals;
+    }
+
+    public void setMinerals(int minerals) {
+        this.minerals = minerals;
+    }
+}
