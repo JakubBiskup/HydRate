d3c2565163bb60e22d08d272e35b8966f980cfab
Jakub Biskup <biskup.jakub@gmail.com>
2021-01-07 22:27:35 +0100

    Add JwtRequestFilter

--- src/main/java/com/example/hydrate/WebSecurityConfig.java
+++ src/main/java/com/example/hydrate/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.hydrate;
 
+import com.example.hydrate.filters.JwtRequestFilter;
 import com.example.hydrate.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +9,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +26,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     MyUserDetailsService userDetailsService;
 
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
@@ -29,19 +37,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf().disable()
+        httpSecurity.cors()
+                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/authenticate").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST,"/water").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST,"/reviews").hasAnyRole("REVIEWER","ADMIN")
                 .antMatchers(HttpMethod.GET,"/reviews/**").hasAnyRole("REVIEWER","ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET,"/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
---
+++ src/main/java/com/example/hydrate/filters/JwtRequestFilter.java
@@ -0,0 +1,66 @@
+package com.example.hydrate.filters;
+
+import com.example.hydrate.service.MyUserDetailsService;
+import com.example.hydrate.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        final String authorizationHeader=request.getHeader("Authorization");
+
+        String jwt=null;
+        String username=null;
+        if(authorizationHeader!=null && authorizationHeader.startsWith("Bearer ")){
+            jwt=authorizationHeader.substring(7);
+            username=jwtUtil.extractUsername(jwt);
+
+        }
+        if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails=this.userDetailsService.loadUserByUsername(username);
+            if(jwtUtil.validateToken(jwt,userDetails)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        boolean shouldNot=false;
+        if(new AntPathMatcher().match("/authenticate", request.getRequestURI())){
+            shouldNot=true;
+        }
+        if(new AntPathMatcher().match("/water/**",request.getRequestURI())){
+            shouldNot=true;
+        }
+        return shouldNot;
+    }
+}
